270f1a6b62acc73c543de8a3990aa950afda9830
Deckel de Lange <deckel.delange@gmail.com>
2019-01-20 13:47:29 +0000

    reorganisation
    changed code around for simplicity, more comments.
    Added limets to scale on output.

--- LidarSystem.py
+++ LidarSystem.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 import random as random
+from mpl_toolkits.mplot3d import Axes3D
 
 #Cartesian to Polar coordinates
 def cartPolar(x, y, z):
@@ -17,10 +18,11 @@ def polarCart(r, theta, phi):
     return(x,y,z)
 #Initialize data
 def random_data(i): 
-    #Define number of points and points in a row
-    #totalPoints needs to be a factor of rowPoints!!!!
-    totalPoints = 200
-    rowPoints = 20
+    #Number of angles measurements are taken
+    rowPoints = 10
+    #Number of LIDAR rangefinders
+    numberRows = 5
+    totalPoints = rowPoints*numberRows
     #Data set length n 
     r = np.ones(totalPoints)
     theta = np.ones(totalPoints)
@@ -29,20 +31,28 @@ def random_data(i):
     for i in range(0,totalPoints,rowPoints):
         for j in range(0,rowPoints):
             theta[i+j] = 2*np.pi/rowPoints * j
-            phi[i+j]   = ((np.pi/2)/(totalPoints/rowPoints)) *i/rowPoints
-            r[i+j] += random.random()*0.1      
+            phi[i+j]   = ((np.pi/2)/(totalPoints/rowPoints)) * i/rowPoints
+           
+            #Lidar entry (random data set for now)
+            r[i+j] += random.random()*0.1 
+
+    #Removing duplicate points for last row
+    r[totalPoints-rowPoints:totalPoints] = r[totalPoints-1]
     #Convert spherical polar coordinates into cartesian
     x, y, z = polarCart(r, theta, phi)
-    #Draw new plot
+    #Draw new plot and set plot paramaters
     ax.clear()
     plt.axis('off')
+    plt.xlim((-1.5,1.5))
+    plt.ylim((-1.5,1.5))
+    ax.set_zlim(0, 1.5)
     ax.plot_trisurf(x,y,z)
     #ax.scatter(x,y,z)
     
-#main
+#Main
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ani = animation.FuncAnimation(fig, random_data, interval=1000)
+ani = animation.FuncAnimation(fig, random_data, interval=100)
 plt.show()
 
 
